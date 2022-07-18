136fb73877430e179aeb4e91a1650d0845c7a89a
Rajneeshkumar12345 <rkv7861999@gmail.com>
2022-07-18 19:00:16 +0530

    add

--- activity/userModel.js
+++ activity/userModel.js
@@ -1,1 +1,10 @@
- let dbLink = "mongodb+srv://Rajneesh:<Mongodb@12345>@freecluster.83r7lqj.mongodb.net/?retryWrites=true&w=majority"
+ const mongoose = require('mongoose')
+ let dblink = "mongodb+srv://Rajneesh:Mongodb%4012345@freecluster.83r7lqj.mongodb.net/?retryWrites=true&w=majority";
+ mongoose 
+       .connect(dblink)
+       .then(function () {
+          console.log("connected");
+       })
+       .catch(function (err) {
+          console.log("error", err);
+       })
